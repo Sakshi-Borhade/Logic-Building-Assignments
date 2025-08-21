d0fcde8562c5134402e3c22df4c2f6bf1bf6eafe
Sakshi-Borhade <borhadesakshi13@gmail.com>
2025-08-21 19:36:37 +0530

    Add files via upload

---
+++ Assignment_48/program2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Pattern
+{
+    public void PatternX(String str)
+    {
+        int i = 0, j = 0;
+
+        for(i = 0; i < str.length(); i++)
+        {
+            for(j = 0; j < i; j++)
+            {
+                System.out.print(str.charAt(j)+"\t");
+            }
+
+            System.out.println();
+        }
+        
+    }
+}
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        Pattern pobj = new Pattern();
+
+        System.out.println("Enter the string : ");
+        String str = sobj.nextLine();
+
+        pobj.PatternX(str);
+    }
+}
